67929c0074203c0b20594365183c3513e69841f4
Namrata Vijay Mali <namratavijaymali@gmail.com>
2026-04-01 14:07:10 -0700

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String s : strs){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String sortedWord = new String(ch);
+
+            if(!map.containsKey(sortedWord)){
+               map.put(sortedWord, new ArrayList<>());
+            }
+            map.get(sortedWord).add(s);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
