f30d4378f89584f07517f0582a6720736db15aad
Namrata Vijay Mali <namratavijaymali@gmail.com>
2026-04-05 13:47:28 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+
+        for(int i=0; i<s.length(); i++){
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+            }else if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+            }else{
+                if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
