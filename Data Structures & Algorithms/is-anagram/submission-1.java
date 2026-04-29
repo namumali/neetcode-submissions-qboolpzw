297c3d19d6c8e5eca6614f088b18234d642921ad
Namrata Vijay Mali <namratavijaymali@gmail.com>
2026-04-29 15:44:47 -0700

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+       Map<Character, Integer> map = new HashMap<>();
+       for(char c : s.toCharArray()){
+        map.put(c, map.getOrDefault(c,0) + 1);
+       }
+
+       for(char c : t.toCharArray()){
+        map.put(c, map.getOrDefault(c, 0)-1);
+       }
+
+       for(int val : map.values()){
+        if(val != 0){
+            return false;
+        }
+       }
+       return  true;
+    }
+}
