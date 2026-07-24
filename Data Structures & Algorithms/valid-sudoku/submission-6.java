4eacbda23b9765456b7c3dd7c8fd549865c30597
Namrata Vijay Mali <namratavijaymali@gmail.com>
2026-07-24 15:48:47 -0700

    Add: valid-sudoku - submission-6

---
+++ Data Structures & Algorithms/valid-sudoku/submission-6.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] subGrids = new boolean[9][9];
+
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                int index = board[i][j] - '1';
+                int subGridIndex = (i/3) * 3 + (j/3);
+
+                if(board[i][j] != '.'){
+                    if(rows[index][i] || cols[index][j] || subGrids[index][subGridIndex]){
+                        return false;
+                    }
+
+                    rows[index][i] = true;
+                    cols[index][j] = true;
+                    subGrids[index][subGridIndex] = true;
+                }
+
+            }
+        }
+        return true;
+    }
+}
